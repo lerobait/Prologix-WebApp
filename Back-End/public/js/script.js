00c6eb857e291f4c4a4647d9bea9ec4506fe20d8
Anna1929 <119493462+Anna1929@users.noreply.github.com>
2024-08-20 00:08:21 +0300

    feat: add theme switcher for light and dark modes
    - Implemented functionality to toggle between light and dark themes.
    - Updated relevant elements to reflect the selected theme.
    - Ensured theme preference is applied consistently across the site.
    - Added favicon.
    - Changed Pug files to support new functionality.

--- Back-End/public/js/script.js
+++ Back-End/public/js/script.js
@@ -1,4 +1,53 @@
+// Author: Anna Geenko
+
 document.addEventListener('DOMContentLoaded', function() {
+    // Switch theme functionality
+    const switchTheme = document.querySelector('.switch input');
+    const elementsToToggle = {
+        body: document.body,
+        header: document.querySelector('header'),
+        footer: document.querySelector('footer'),
+        logo: document.querySelector('.logo img'),
+        links: document.querySelectorAll('a'),
+        text: document.querySelectorAll('p'),
+        icons: document.querySelectorAll('.icon'),
+        filterButtons: document.querySelectorAll('.filter-button, .sub-filter-button'),
+        filters: document.querySelector('#filters'),
+        categories: document.querySelector('#categories'),
+        imgTitle: document.querySelectorAll('.img-title'),
+        listImg: document.querySelector('#list-images'),
+        scroll: document.querySelector('#scroll-to-top'),
+        mobileMenu: document.querySelector('.mobile-menu'),
+        textForMobile: document.querySelectorAll('.mobile-menu a')
+    };
+
+    // Check if the user changed the theme preference
+    switchTheme.addEventListener('change', function() {
+        const isLightMode = this.checked;
+        elementsToToggle.body.classList.toggle('light', isLightMode);
+        elementsToToggle.header.classList.toggle('light', isLightMode);
+        elementsToToggle.footer.classList.toggle('light', isLightMode);
+        elementsToToggle.logo.src = isLightMode ? './img/logo-black.png' : './img/logo.png';
+        elementsToToggle.links.forEach(link => link.classList
+            .toggle('light', isLightMode));
+        elementsToToggle.text.forEach(p => p.classList
+            .toggle('light', isLightMode));
+        elementsToToggle.icons.forEach(icon => icon.classList
+            .toggle('light', isLightMode));
+        elementsToToggle.filterButtons.forEach(button => button.classList
+            .toggle('light', isLightMode));
+        elementsToToggle.filters.classList.toggle('light', isLightMode);
+        elementsToToggle.categories.classList.toggle('light', isLightMode);
+        elementsToToggle.imgTitle.forEach(title => title.classList
+            .toggle('light', isLightMode));
+        elementsToToggle.listImg.classList.toggle('light', isLightMode);
+        elementsToToggle.scroll.classList.toggle('light', isLightMode);
+        elementsToToggle.mobileMenu.classList.toggle('light', isLightMode);
+        elementsToToggle.textForMobile.forEach(text => text.classList
+            .toggle('light', isLightMode));
+    });
+
+
     // Function to update the height of the filters section based on the active filters
     function updateFiltersHeight(filterButton, filtersSection) {
         const activeFilterList = document.querySelector('.filter-list.show');
