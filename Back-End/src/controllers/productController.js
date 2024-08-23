8776efa0f8d1aa06828282d9f82b017c0c886d99
Nikulin Artem <48065996+lerobait@users.noreply.github.com>
2024-08-23 19:38:19 +0300

    feat(product): render product page on server-side
    - Added `card.pug` template for displaying product details.
    - Implemented `getProductByCode` function in `productsController` to fetch product data by code.
    - Created a new route in `productRoutes` to handle requests for individual product pages.
    - Added `addProductCardEventListeners` function in `script.js` to handle interactive events on product cards.

--- Back-End/src/controllers/productController.js
+++ Back-End/src/controllers/productController.js
@@ -9,13 +9,13 @@ class ProductController {
    * Retrieves products from the database based on specified categories and optional search query.
    * It supports filtering by multiple categories, which are passed as a comma-separated list in the query parameter `categories`
    * It also supports pagination via `page` and `limit` query parameters.
-   * 
+   *
    * @param {Object} req - The request object containing details of the HTTP request.
    * @param {Object} res - The response object used to send a response to the client.
    * @returns {Promise<void>} - A promise that resolves when the response has been sent.
-   * 
+   *
    * @throws {AppError} - Throws an AppError if there is an issue with the database query or other processing errors.
-   * 
+   *
    * @example
    * Example request: GET /?categories=2,5,8&page=1&limit=15
    * This will fetch products with category_id 2, 5, or 8, with pagination on page 1 and limit 40 products per page.
@@ -23,7 +23,9 @@ class ProductController {
   async getProducts(req, res) {
     const { categories, page = 1, limit = 15 } = req.query;
     const offset = (page - 1) * limit;
-    const params = req.query.categories ? `?categories=${req.query.categories}` : '';
+    const params = req.query.categories
+      ? `?categories=${req.query.categories}`
+      : '';
     let render = 'overview';
 
     let query = 'SELECT * FROM products';
@@ -46,15 +48,20 @@ class ProductController {
       // No categories, so LIMIT and OFFSET use $1 and $2
       query += ' LIMIT $1 OFFSET $2';
       queryParams.push(Number(limit), Number(offset));
-}
+    }
 
     try {
       // Query the database to fetch all products
       const products = await db.any(query, queryParams);
 
       // Get total count for pagination info
-      const totalCountQuery = 'SELECT COUNT(*) FROM products' + (categories ? ' WHERE category_id = ANY($1::int[])' : '');
-      const totalCountResult = await db.one(totalCountQuery, categories ? [categories.split(',').map(Number)] : []);
+      const totalCountQuery =
+        'SELECT COUNT(*) FROM products' +
+        (categories ? ' WHERE category_id = ANY($1::int[])' : '');
+      const totalCountResult = await db.one(
+        totalCountQuery,
+        categories ? [categories.split(',').map(Number)] : []
+      );
       const totalCount = parseInt(totalCountResult.count, 10);
 
       res.status(200).render(render, {
@@ -79,23 +86,25 @@ class ProductController {
   }
 
   /**
-    * Searches for products based on a search query.
-    * Supports pagination via `page` and `limit` query parameters.
-    * 
-    * @param {Object} req - The request object containing details of the HTTP request.
-    * @param {Object} res - The response object used to send a response to the client.
-    * @returns {Promise<void>} - A promise that resolves when the response has been sent.
-    * 
-    * @throws {AppError} - Throws an AppError if there is an issue with the database query or other processing errors.
-    * 
-    * @example
-    * Example request: GET products/search?search=laptop&page=1&limit=15
-    * This will fetch products where the title contains 'laptop', with pagination on page 1 and limit 40 products per page.
-    */
+   * Searches for products based on a search query.
+   * Supports pagination via `page` and `limit` query parameters.
+   *
+   * @param {Object} req - The request object containing details of the HTTP request.
+   * @param {Object} res - The response object used to send a response to the client.
+   * @returns {Promise<void>} - A promise that resolves when the response has been sent.
+   *
+   * @throws {AppError} - Throws an AppError if there is an issue with the database query or other processing errors.
+   *
+   * @example
+   * Example request: GET products/search?search=laptop&page=1&limit=15
+   * This will fetch products where the title contains 'laptop', with pagination on page 1 and limit 40 products per page.
+   */
   async searchProducts(req, res) {
     const { search, page = 1, limit = 15 } = req.query;
     const offset = (page - 1) * limit;
-    const params = req.query.search ? `/products/search?search=${req.query.search}` : '';
+    const params = req.query.search
+      ? `/products/search?search=${req.query.search}`
+      : '';
 
     let query = 'SELECT * FROM products';
     const queryParams = [];
@@ -113,8 +122,13 @@ class ProductController {
       const products = await db.any(query, queryParams);
 
       //Get total count for pagination info
-      const totalCountQuery = 'SELECT COUNT(*) FROM products' + (search ? ' WHERE title ILIKE $1' : '');
-      const totalCountResult = await db.one(totalCountQuery, search ? [`%${search}%`] : []);
+      const totalCountQuery =
+        'SELECT COUNT(*) FROM products' +
+        (search ? ' WHERE title ILIKE $1' : '');
+      const totalCountResult = await db.one(
+        totalCountQuery,
+        search ? [`%${search}%`] : []
+      );
       const totalCount = parseInt(totalCountResult.count, 10);
 
       res.status(200).render('overview', {
@@ -122,7 +136,7 @@ class ProductController {
         products,
         totalCount,
         totalPages: Math.ceil(totalCount / limit),
-        currentPage: parseInt(page, 10)
+        currentPage: parseInt(page, 10),
       });
     } catch (error) {
       // Handle errors that occur during the database query
@@ -131,6 +145,34 @@ class ProductController {
         res.status(error.statusCode).json({ error: error.message });
       } else {
         // For other errors, create a generic AppError and send a 500 Internal Server Error response
+        const appError = new AppError('Internal Server Error', 500);
+        res.status(appError.statusCode).json({ error: appError.message });
+      }
+    }
+  }
+
+  async getProductByCode(req, res) {
+    // Extract the product code from the request parameters
+    const { code } = req.params;
+
+    try {
+      // Fetch the product details from the database using the provided code
+      const product = await db.one('SELECT * FROM products WHERE code = $1', [
+        code,
+      ]);
+
+      // Render the 'card' template with the product details
+      res.status(200).render('card', {
+        title: `Товар - ${product.title}`, // Set the page title with the product's title
+        product, // Pass the product data to the template
+      });
+    } catch (error) {
+      // Check if the error is an instance of AppError (a custom error class)
+      if (error instanceof AppError) {
+        // If it's a known application error, send the appropriate status and error message
+        res.status(error.statusCode).json({ error: error.message });
+      } else {
+        // If it's an unknown error, create a generic internal server error response
         const appError = new AppError('Internal Server Error', 500);
         res.status(appError.statusCode).json({ error: appError.message });
       }
