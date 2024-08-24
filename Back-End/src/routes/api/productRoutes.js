d52b8804361d5eda1b26ba69de8bbbef3bf8a7d3
Nikulin Artem <48065996+lerobait@users.noreply.github.com>
2024-08-24 15:53:03 +0300

    docs(api): add Swagger documentation for product routes
    - Added Swagger documentation for all endpoints in `productRoutes.js`.
    - Included descriptions, parameters, and response examples for each route.

--- Back-End/src/routes/api/productRoutes.js
+++ Back-End/src/routes/api/productRoutes.js
@@ -4,13 +4,133 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/productController');
 
-// Define a route for GET requests to '/products'
+/**
+ * @swagger
+ *
+ * tags:
+ *  name: Products
+ *  description: Operations related to products
+ * */
+
+/**
+ * @swagger
+ *
+ * /api/products:
+ *  get:
+ *    tags: [Products]
+ *    description: Get a list of products based on query parameters
+ *    parameters:
+ *      - name: categories
+ *        in: query
+ *        description: Comma-separated list of category IDs
+ *        required: false
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Successful response with a list of products
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                products:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: integer
+ *                        description: Product ID
+ *                      name:
+ *                        type: string
+ *                        description: Product name
+ *                      price:
+ *                        type: number
+ *                        format: float
+ *                        description: Product price
+ *      400:
+ *        description: Invalid query parameters
+ */
 router.get('/products', productController.getProducts);
 
-// Route for searching products
+/**
+ * @swagger
+ *
+ * /api/products/search:
+ *  get:
+ *    tags: [Products]
+ *    description: Search for products based on a search query
+ *    parameters:
+ *      - name: search
+ *        in: query
+ *        description: Search term
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Successful response with a list of products matching the search query
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                products:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: integer
+ *                        description: Product ID
+ *                      name:
+ *                        type: string
+ *                        description: Product name
+ *                      price:
+ *                        type: number
+ *                        format: float
+ *                        description: Product price
+ *      400:
+ *        description: Invalid search parameters
+ */
 router.get('/products/search', productController.searchProducts);
 
-// Route for get one product by code
+/**
+ * @swagger
+ *
+ * /api/products/{code}:
+ *  get:
+ *    tags: [Products]
+ *    description: Get a single product by its code
+ *    parameters:
+ *      - name: code
+ *        in: path
+ *        description: Product code
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Successful response with product details
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: integer
+ *                  description: Product ID
+ *                name:
+ *                  type: string
+ *                  description: Product name
+ *                price:
+ *                  type: number
+ *                  format: float
+ *                  description: Product price
+ *      404:
+ *        description: Product not found
+ */
 router.get('/products/:code', productController.getProductByCode);
 
 module.exports = router;
